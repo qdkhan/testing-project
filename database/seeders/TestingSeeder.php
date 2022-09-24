b59663e398f448ccc17c16fb2d6b7cdd00c1cd22
qdkhan <qutbuddin.khan@xipetech.com>
2022-09-24 15:50:50 +0530

    seeder

--- database/seeders/TestingSeeder.php
+++ database/seeders/TestingSeeder.php
@@ -15,12 +15,23 @@ class TestingSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('testings')->insert([
-            'name'      => 'Ayan Khan',
-            'address'   => 'Mumbai',
-            'dob'       => '2017-06-02',
-            'movies'    => '["F","G","H","I"]',
-            'created_at'=> date('Y-m-d H:i:s')
-        ]);
+        $faker = \Faker\Factory::create();
+        // DB::table('testings')->insert([
+        //     'name'      => 'Ayan Khan',
+        //     'address'   => 'Mumbai',
+        //     'dob'       => '2017-06-02',
+        //     'movies'    => '["F","G","H","I"]',
+        //     'created_at'=> date('Y-m-d H:i:s')
+        // ]);
+        for($i=0; $i<5; $i++){
+                DB::table('testings')->insert([
+                    'name'      => $faker->name(),
+                    'address'   => $faker->city(),
+                    'dob'       => $faker->date($format = 'Y-m-d', $max = 'now'),
+                    'movies'    => '["FF","GG","HH","II"]',
+                    'created_at'=> date('Y-m-d H:i:s'),
+                    'updated_at'=> date('Y-m-d H:i:s')
+                ]);
+        }
     }
 }
