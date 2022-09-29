aebd0a56a9f0393439f4ccac9240d659bb558cb9
qdkhan <qutbuddin.khan@xipetech.com>
2022-09-29 15:26:03 +0530

    Changes

--- resources/views/registration-form.blade.php
+++ resources/views/registration-form.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    
     <title>Document</title>
 </head>
 <body>
@@ -69,11 +69,5 @@
         </tbody>
         @endif
     </table>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script>
-        $(document).ready( function () {
-            $('#myTable').DataTable();
-        } );
-    </script>
 </body>
 </html>
