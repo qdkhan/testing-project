902c76ff0ab02a31f2b77de2bd25608d804d0ad9
qdkhan <qutbuddin.khan@xipetech.com>
2022-09-29 10:50:22 +0530

    Testing

--- config/app.php
+++ config/app.php
@@ -194,7 +194,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Yajra\DataTables\DataTablesServiceProvider::class,
 
     ],
 
