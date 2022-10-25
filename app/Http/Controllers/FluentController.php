896f9b73be12c70302ee73bebebab0b55ed18935
qdkhan <qutbuddin.khan@xipetech.com>
2022-10-25 18:58:07 +0530

    Testing

--- app/Http/Controllers/FluentController.php
+++ app/Http/Controllers/FluentController.php
@@ -13,6 +13,19 @@ class FluentController extends Controller
         $slice = Str::of('Hello And Welcome')->after('Hello');
         // return $slice;
         $slice = Str::of('\Illuminate\Http\Request')->afterLast('\\');
+
+        $slice = Str::of('\Illuminate\Http\Request')->append('\Xipetech');
+
+        $slice = Str::of('\Illuminate\Http\Request')->lower();
+
+        $slice = Str::of('\Illuminate\Http\Request')->replace('\Request','\Not Requested');
+
+        $slice = Str::of('\illuminate\http\request')->replace('\request','\not requested')->title();
+
+        $slice = Str::of('Hello And Welcome')->slug();
+
+        $slice = Str::of('Hello And Welcome')->substr('6','5');
+
         return $slice;
     }
     
