74b4e198f2347f6733ea20c993170c5d33f4fa5d
qdkhan <qutbuddin.khan@xipetech.com>
2022-10-27 14:18:22 +0530

    Form Validation

--- resources/views/registration-form.blade.php
+++ resources/views/registration-form.blade.php
@@ -100,5 +100,17 @@
             </div>
         </div>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    @if(session()->has('success'))
+        <script>
+            swal('Great Job !!', "{!! session()->get('success') !!}", "success",{ button:'Ok'});
+        </script>
+    @endif
+
+    @if(session()->has('delete'))
+        <script>
+            swal('Great Job !!', "{!! session()->get('delete') !!}", "error",{ button:'Ok'});
+        </script>
+    @endif
 </body>
 </html>
